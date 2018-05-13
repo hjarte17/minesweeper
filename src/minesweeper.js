432b7757c5d3e0e54827b10b3d723273fe99c80e
Hector <hjarte1@hotmail.com>
2018-05-13 16:34:08 -0400

    Printed the game  board with less hardcode

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,19 @@
-// Run node minesweeper.js to run this code.
-// Hardcode 3 blank columns to represent empty lots
-const blankLine = '  |   |  ';
-console.log("This is what an empty board would look like:");
-// Log the blankLine in 3 rows
-console.log(`${blankLine} \n${blankLine} \n${blankLine}`);
+//Prints out the board
+const printBoard = board => {
+    console.log('Current Board:');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
+//sets the rows and columns of the borad
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
 
-
-// Hardcode a guessline
-const guessLine = '1 |   |  ';
-// harcode a bombline
-const bombLine = '  | B |  ';
-// Print what a board might look like during a game.
-console.log("This is what a board with a guess and a bomb on it would look like:");
-// Print a guessLine, a bombLine and a blankLine.
-console.log(`${guessLine} \n${bombLine} \n${blankLine}`);
+// Manually hardcoded
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
